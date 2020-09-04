1a1e12c58bdc5f5fb76f461e241610107d0a59ee
Alfonso <alfof87@gmail.com>
2020-09-04 16:24:33 +0200

    php-ajax-dischi

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
   include 'vars.php';
   // echo $data["response"]["title"];
+  // echo count($data["response"]);
 
-  foreach ($data as $arr) {
+  $dataResponse = $data["response"];
+  echo $dataResponse;
+  echo count($dataResponse);
+
+  foreach ($dataResponse as $arr) {
     echo $arr["poster"] . "<br>";
     echo $arr["title"] . "<br>";
     echo $arr["author"] . "<br>";
     echo $arr["genre"] . "<br>";
     echo $arr["year"] . "<br>";
+    echo "<br><br>";
   }
 
  ?>
