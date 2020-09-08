77ea57bccddda0a8efc91b59a2084365296a7982
Alfonso <alfof87@gmail.com>
2020-09-08 16:57:11 +0200

    php-ajax-dischi

--- ajax/data.php
+++ ajax/data.php
@@ -78,5 +78,5 @@
             ];
 
 
-  echo json_encode($data);
+  // echo json_encode($data);
   ?>
--- ajax/script.js
+++ ajax/script.js
@@ -2,8 +2,20 @@ function getData(){
   $.ajax({
     url: "data.php",
     method: "GET",
+    data: {
+      "poster" : poster,
+      "title" : title,
+      "author" : author,
+      "genre" : genre,
+      "year" : year
+    },
     success: function(data){
-      console.log(data);
+      // console.log(data);
+      var template = $("#disk-template").html();
+      var compiled = Handlebars.compile(template);
+      var target = $("#disk ul");
+      var diskHTML = compiled(disk);
+      target.append(diskHTML);
     },
     error: function(error){
       console.log(error);
